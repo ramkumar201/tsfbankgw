1d3567037fc47eabbe475965e86ced62b531057a
ramkumar201 <70479174+ramkumar201@users.noreply.github.com>
2021-01-09 14:43:52 +0530

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" href="css/footer.css">    
+<center>
+    <div class="social">
+        <div class="social-icon">
+            <a href="#" target=_blank><img src="image/footer/facebook.svg"></a>
+            <a href="#"><img src="image/footer/instagram.svg"></a>
+            <a href="#"><img src="image/footer/twitter.svg"></a>
+            <a href="#"><img src="image/footer/github.svg"></a>
+        </div>
+        <div class="footer">
+            <span class="footer-left">&copy; 2021 The Sparks Foundation || </span><label>Developed by Ramkumar</label>
+        </div>
+    </div>
+</center>
